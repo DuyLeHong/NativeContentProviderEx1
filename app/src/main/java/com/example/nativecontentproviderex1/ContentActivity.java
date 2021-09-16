bc500eb79b3a564eb10bfaf5c49dfbba9f43fd4e
duyle <leduy.av22@gmail.com>
2021-09-16 20:54:38 +0700

    //update Handler

--- app/src/main/java/com/example/nativecontentproviderex1/ContentActivity.java
+++ app/src/main/java/com/example/nativecontentproviderex1/ContentActivity.java
@@ -5,9 +5,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,25 +22,54 @@ public class ContentActivity extends AppCompatActivity {
     URL ImageUrl = null;
     InputStream is = null;
     Bitmap bmImg = null;
-    ImageView imageView= null;
+    ImageView imageView = null;
     ProgressDialog p;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout2);
 
-        Button button=findViewById(R.id.asyncTask);
-        imageView=findViewById(R.id.image);
+        Button button = findViewById(R.id.asyncTask);
+        imageView = findViewById(R.id.image);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AsyncTaskExample asyncTask=new AsyncTaskExample();
-                asyncTask.execute("https://codefresher.vn/wp-content/uploads/2021/06/Banner-01-QC-Android-1024x1024.png");
+                AsyncTaskExample asyncTask = new AsyncTaskExample();
+                asyncTask.execute("https://codefresher.vn/wp-content/uploads/2021/06/banner-Java-core-03-1024x1024.png");
             }
         });
+
+        final Handler h = new Handler();
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "AAA", Toast.LENGTH_SHORT).show();
+            }
+        }, 5000);
+
+        h.post(new Runnable() {
+            @Override
+            public void run() {
+                //abc
+
+                h.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+            }
+        });
+
+        //TimerTask Android
     }
+
     private class AsyncTaskExample extends AsyncTask<String, Void, Bitmap> {
+
+
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -48,6 +79,7 @@ public class ContentActivity extends AppCompatActivity {
             p.setCancelable(false);
             p.show();
         }
+
         @Override
         protected Bitmap doInBackground(String... strings) {
             try {
@@ -59,18 +91,21 @@ public class ContentActivity extends AppCompatActivity {
                 BitmapFactory.Options options = new BitmapFactory.Options();
                 options.inPreferredConfig = Bitmap.Config.RGB_565;
                 bmImg = BitmapFactory.decodeStream(is, null, options);
+
+                //publishProgress(50);
             } catch (IOException e) {
                 e.printStackTrace();
             }
             return bmImg;
         }
+
         @Override
         protected void onPostExecute(Bitmap bitmap) {
             super.onPostExecute(bitmap);
-            if(imageView!=null) {
+            if (imageView != null) {
                 p.hide();
                 imageView.setImageBitmap(bitmap);
-            }else {
+            } else {
                 p.show();
             }
         }
